99ec504014853ae881144e0f2e51ab133d553fcc
Tomasz Cieślik <tc519@interia.pl>
2021-01-08 06:05:38 +0100

    Dodanie dokumentacji oraz programu po realizacji pierwszego etapu projektu

---
+++ RzutMoneta/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RzutMoneta
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            String gra = "Tak";
+      
+            Console.WriteLine("Program symulujący rzut monetą");
+           
+            do
+            {
+                int LMonet=0;
+                do
+                {
+                    Console.WriteLine("Ile monet? – wpisz liczbę w przedziale od 1 do 5");
+                    String line = Console.ReadLine();
+
+                    try
+                    {
+                        LMonet = Int32.Parse(line);
+                        if (LMonet <= 0 || LMonet > 5) 
+                        {
+                            throw new ArgumentException();
+                        }
+
+                    }
+                    catch (FormatException )
+                    {
+                        Console.WriteLine("!! Wprowadzonym znakiem nie jest liczba całkowita !!");
+                
+                     
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("!! Wprowadzona liczba jest spoza zakresu 1-5 !!");
+                        LMonet = 0;
+                     
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    
+                    }
+               
+
+
+                } while (LMonet < 1 || LMonet > 5);
+
+           
+
+                Rzut rz = new Rzut();
+                rz.Losowanie(LMonet);
+                rz.Wyniki();
+                Console.WriteLine("\nCzy chcesz jeszcze raz zagrać? (wpisz \"Nie\" jeśli chcesz zakończyć),następnie zatwierdź)");
+                if (Console.ReadLine()=="Nie")
+                gra = "Nie";
+            } while (gra == "Tak");
+            Console.WriteLine("Dziękujemy za skorzystanie z programu" +
+                " - aby zamknąć program naciśnij Enter");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RzutMoneta/Rzut.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RzutMoneta
+{
+    public class Rzut
+    {
+        public Rzut()
+        {
+        }
+        int[] values;
+        int orly = 0;
+        int reszki = 0;
+
+        public void Losowanie(int x)
+        {
+            Random losuj = new Random();
+            values = new int[x];
+            for (int i = 0; i < x; i++)
+            {
+                values[i] = losuj.Next(0, 2);
+             
+            }
+        }
+
+
+        public void Wyniki()
+        {
+
+          //  Console.WriteLine("\nWykonano {0} rzutów monetą.", values.Length);
+            Console.Write("\nWylosowano:");
+            for (int j = 0; j < values.Length; j++)
+            {
+                int w = values[j];
+                switch (w)
+                {
+                    case 0:
+                        Console.Write("     "+"[RESZKA]");
+                        break;
+                    case 1:
+                        orly += 1;
+                        Console.Write("     "+"[ORZEŁ]");
+                        break;
+                    default:
+                        continue;
+                }
+            }
+
+            reszki = values.Length - orly;
+            Console.WriteLine("\n\n" + $"ORZEŁ: {orly}");
+            Console.WriteLine($"RESZKA: {reszki}");
+
+
+        }
+
+    }
+
+}
